436a79dae0db37bbfd96e09e6c22cc366103440d
anupamaAeonix <ahalder@aeonixinnovations.com>
2024-04-17 12:01:55 +0530

    test commit

--- web_client_javascript/src/components/pages/home/Home.jsx
+++ web_client_javascript/src/components/pages/home/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Chatbot from "../../shared/chatbot/chatbotMain/Chatbot";
 
 const Home = () => {
-  console.log("This is the main chatbot file");
   return (
     <div className="home-main-container" id="home-main-container">
       <Chatbot></Chatbot>
